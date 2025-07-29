12e59796503360f64f403bd752e00312351fd42e
Santiago Lamas <snlamas@gmail.com>
2025-07-28 22:57:36 -0600

    fix: import Http404

--- watchlist/views.py
+++ watchlist/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
+from django.http import Http404
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.views import LoginView
 from .forms import UserForm
